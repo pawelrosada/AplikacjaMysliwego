725c265aa8917ed2154770a1db36c4fac6434288
amocarski <admocarski@gmail.com>
2016-05-15 16:31:26 +0200

    bug fix - mapa

--- app/src/main/java/com/example/am/aplikacjamyliwego/MainActivity.java
+++ app/src/main/java/com/example/am/aplikacjamyliwego/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     //GPS example activity start
                     Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                    startActivity(intent);
                 }
             });
         }
